7db4e22e363c5ec3260e7c0fd90b4a9cdea956b2
Erick_Verissim0 <estudodoerick@gmail.com>
2023-04-28 15:22:48 -0300

    put and patch

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,37 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Patch } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
-  @Post()
+  @Post()  // quando não tem virgula é porque ele está decorando e passando para o argumento depois do decorator
   async create(@Body() body) {
     return { body };
   }
 
   @Get()
   async read() {
-    return{users:[]}
+    return { users: [] }
   }
 
   @Get(':id')
   async readOne(@Param() params) {
-    return {user: {}, params}
+    return { user: {}, params }
+  }
+
+  @Put(':id')
+  async updateAll(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params
+    }
+  }
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params
+    }
   }
 
 }
